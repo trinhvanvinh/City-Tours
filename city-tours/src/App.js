4203ca80cce7be51d4a141d81db459309013d128
mac <>
2019-05-14 17:26:18 +0700

    resource

---
+++ city-tours/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import NavBar from '../src/components/navbar/NavBar.js';
+
+function App() {
+  return (
+    <NavBar/>
+  );
+}
+
+export default App;
