b5f1d8f1019537dcda4f396461c23b8cfe952122
trinhvanvinh <trinhvanvinh018@gmail.com>
2019-05-14 23:15:40 +0700

    no message

--- city-tours/src/App.js
+++ city-tours/src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import NavBar from '../src/components/navbar/NavBar.js';
+import TourList from './components/TourList';
 
 function App() {
   return (
-    <NavBar/>
+    <main>
+      <NavBar/>
+      <TourList/>
+    </main>
+    
   );
 }
 
---
+++ city-tours/src/components/TourList/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './TourList.scss';
+
+export default class TourList extends Component {
+  render() {
+    return (
+      <section>
+        trinhadmasl
+      </section>
+    )
+  }
+}
